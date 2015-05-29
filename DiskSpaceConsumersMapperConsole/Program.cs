aeb66b8037b193d8b7e153e9550c11bab51d2e08
maximburyak <maxim@ayende.com>
2015-05-29 20:09:52 +0300

    Initial commit

---
+++ DiskSpaceConsumersMapperConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using FreeSpaceFinderEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FressSpaceFinderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? minimumThreashold = null;
+            if (args.Length == 3)
+            {
+                minimumThreashold = Int32.Parse(args[2]);
+            }
+            var result = new Crawler().ExecuteCrawling(args[0], minimumThreashold, Int32.Parse(args[1]));
+            var res2 = result;
+            var stream = File.OpenWrite(string.Format("FSFR_min_stdev_{0}.txt", args[1]));
+            var presentation = Encoding.Unicode.GetBytes(string.Format("Summary for directory {0}, with standart deviation of {1}", args[0], args[1]));
+            stream.Write(presentation, 0, presentation.Length);
+            PrintResults(result, stream, 0);
+            stream.Flush();
+            stream.Close();
+        }
+        private static byte[] newLineBytes = Encoding.Unicode.GetBytes("\n");
+        private static byte[] tabBytes = Encoding.Unicode.GetBytes("\t");
+        private static void PrintResults(FreeSpaceFinderEngine.Crawler.ItemDetails curItem, FileStream stream, int depth = 0)
+        {
+            stream.Write(newLineBytes, 0, newLineBytes.Length);
+
+            for (var i=0; i< depth; i++)
+            {                
+                stream.Write(tabBytes,0,tabBytes.Length);
+            }
+
+            var nameInBytes = Encoding.Unicode.GetBytes(string.Format("{0} ({1:N} KB)",curItem.Name, (double)curItem.Size/1024));            
+            stream.Write(nameInBytes, 0, nameInBytes.Length);
+
+            var curItemAsDirectory = curItem as FreeSpaceFinderEngine.Crawler.DirectoryDetails;
+            if (curItemAsDirectory != null)
+            {
+                var directoryDetails = Encoding.Unicode.GetBytes(string.Format(" - childrenCount: {0} mean child size: {1:N} KB", curItemAsDirectory.ChildrenCount, curItemAsDirectory.ChildrenMeanSize/1024));
+                stream.Write(directoryDetails, 0, directoryDetails.Length);
+                foreach (var child in curItemAsDirectory.Children)
+                {
+                    stream.Write(newLineBytes, 0, newLineBytes.Length);
+                    PrintResults(child, stream, depth + 1);                    
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ DiskSpaceConsumersMapperEngine/Crawler.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeSpaceFinderEngine
+{
+    public class Crawler
+    {
+        private int _minStdDevCount;
+        public class ItemDetails
+        {
+            public ItemDetails(ItemDetails parent, string name)
+            {
+                Parent = parent;
+                Name = name;
+                if (parent == null)
+                    Path = name;                
+                else
+                    Path = string.Concat(parent.Path, "/", name);
+                
+            }
+            public readonly string Path;
+            public ItemDetails Parent { get; set; }
+            public readonly string Name;
+            public long Size { get; set; }            
+        }
+        public class DirectoryDetails : ItemDetails
+        {
+            public DirectoryDetails(ItemDetails parent, string name):base(parent,name)
+            {
+
+            }
+            public List<ItemDetails> Children;
+            public bool AnyChildrenRelevant = false;
+
+            public int ChildrenCount { get; set; }
+
+            public double ChildrenMeanSize { get; set; }
+
+            public double ChildrenStandartDeviation { get; set; }
+        }
+        private int _minimumThreshold = 1024*1024*10;
+        public DirectoryDetails ExecuteCrawling(string rootPath, int? minimumThreshold, int minStdDevCount = 3)
+        {
+            _minStdDevCount = minStdDevCount;
+            _minimumThreshold = minimumThreshold.HasValue?  minimumThreshold.Value: 1024 * 1024 * 10;
+            DirectoryDetails result = null;
+            try
+            {
+                DirectoryInfo rootPathDirectory = new DirectoryInfo(rootPath);
+                result = new DirectoryDetails(null, rootPath)
+                {                    
+                    Size = 0
+                };
+                ProcessDirectory(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Errorous root path:{0}, exception: {1}", rootPath, ex.Message);
+            }
+            return result;
+        }
+
+        private void ProcessDirectory(DirectoryDetails curItem)
+        {
+            try
+            {
+                List<ItemDetails> children = new List<ItemDetails>();
+                DirectoryInfo curDirectory = new DirectoryInfo(curItem.Path);
+                var childFiles = curDirectory.GetFiles();
+                var childDirectories = curDirectory.GetDirectories();
+
+                children.AddRange(childDirectories.Select(x =>
+                {
+                    DirectoryDetails newDirectory = new DirectoryDetails(curItem, x.Name);
+                    ProcessDirectory(newDirectory);
+                    curItem.AnyChildrenRelevant = curItem.AnyChildrenRelevant | newDirectory.AnyChildrenRelevant;
+                    return newDirectory;
+                }).ToList());
+
+                children.AddRange(childFiles.Select(x => new ItemDetails(curItem, x.Name)
+                {   
+                    Size = x.Length
+                }).ToList());
+
+                curItem.Size = children.Sum(x => x.Size);
+                if (children.Count == 0)
+                    return;
+                double childrenMeanSize = (double)curItem.Size / children.Count;
+                double childrenStandartDeviation = (double)Math.Pow(children.Sum(x => Math.Pow(x.Size - childrenMeanSize, 2)) / children.Count, 0.5);
+                long childrenMaxSize = children.Max(x => x.Size);
+
+                double maxItemStandartDeviationDistanceFromMean = (childrenMaxSize - childrenMeanSize) / childrenStandartDeviation;
+                curItem.ChildrenCount = children.Count;
+                if (maxItemStandartDeviationDistanceFromMean > _minStdDevCount)
+                {
+                    children = children.Where(x => x.Size >= _minimumThreshold && ( x.Size - childrenMeanSize >= _minStdDevCount || x is DirectoryDetails && (x as DirectoryDetails).AnyChildrenRelevant )).ToList();
+                }
+                else
+                {
+                    children = children.Where(x => x.Size >= _minimumThreshold &&  x is DirectoryDetails && (x as DirectoryDetails).AnyChildrenRelevant).ToList();
+                }
+
+                curItem.ChildrenMeanSize = childrenMeanSize;
+                curItem.ChildrenStandartDeviation = childrenStandartDeviation;               
+                curItem.Children = children;
+                curItem.AnyChildrenRelevant |= children.Any();
+            }
+            catch (System.UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Unauthorized Exception occured in directory {0}. Exception message: {1}", curItem.Path, ex.Message);
+            }
+        }
+    }
+}
